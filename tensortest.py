25ed16b848b0838c9bd566176e01da06ff6caff4
WilliamCastner <williambcastner@gmail.com>
2025-10-12 01:30:30 -0400

    Added gitignore and figured out custom dataset to use -> tomorrow follow comptuer vision tutorial according to the 2011 bird dataset.

--- tensortest.py
+++ tensortest.py
@@ -1,12 +1,38 @@
-import itertools
-import os
-
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 import numpy as np
-
+import PIL
 import tensorflow as tf
-import tensorflow_hub as hub
 
-print("TF version:", tf.__version__)
-print("Hub version:", hub.__version__)
-print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+import pathlib
+
+dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/CUB_200_2011.tgz"
+data_dir = tf.keras.utils.get_file('CUB_200_2011.tar', origin=dataset_url, extract=True)
+data_dir = pathlib.Path(data_dir).with_suffix('')
+
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
